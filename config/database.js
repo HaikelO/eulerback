863874a0e9da62d5b4343a4cbf508bd02bd17b2e
Haikel O <Haikel.ben@gmail.com>
2018-11-25 20:52:33 +0100

    db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-   url: 'mongodb://localhost:27017/euler',
+   /* url: 'mongodb://localhost:27017/euler', */
+   url: 'mongodb://dbeuler:dbeulerpass1@ds117164.mlab.com:17164/heroku_jbqwkkm5'
 };
 
 
