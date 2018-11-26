a5b24021913d0e6ece6f42782e51502368fc729b
HaikelO <haikel.ben@gmail.com>
2018-11-26 22:43:58 +0100

    fix chatroutes

--- src/chat/routes/index.js
+++ src/chat/routes/index.js
@@ -1,5 +1,5 @@
 const ChatRoutes = (app) => {
-    app.use('/api/messages', (req, res)=>{
+    app.get('/api/messages', (req, res)=>{
         console.log('/messages');
         res.json([{message:'Bonjour !'}, {message:'Hello !'}, {message:'C\'est bien ...'}]);
     });
